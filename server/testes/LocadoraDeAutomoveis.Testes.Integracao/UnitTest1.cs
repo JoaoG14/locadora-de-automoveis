6c9abc60a95a82975fcaac54e6954d5827bace75
João <joaog.alves14@gmail.com>
2026-02-12 10:26:48 -0300

    Adiciona testes de integração e configurações do projeto

---
+++ server/testes/LocadoraDeAutomoveis.Testes.Integracao/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace LocadoraDeAutomoveis.Testes.Integracao;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
--- server/web-api/Program.cs
+++ server/web-api/Program.cs
@@ -145,3 +145,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
